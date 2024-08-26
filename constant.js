e254f8307f27dbef717254f23ce40eb0233a9bcc
DhanushaRenganathan <161591318+DhanushaRenganathan@users.noreply.github.com>
2024-08-26 20:46:35 +0530

    Add files via upload

---
+++ constant.js
@@ -0,0 +1,28 @@
+export const logo = 'youtube.png';
+
+export const categories = [
+  { name: 'New', icon: 'home.png', },
+  { name: 'JS', icon: 'js.png', },
+  { name: 'Coding', icon: 'code.png', },
+  { name: 'ReactJS', icon: 'react js.png', },
+  { name: 'NextJS', icon: 'next.png', },
+  { name: 'Music', icon: 'music.png', },
+  { name: 'Education', icon: 'education.png', },
+  { name: 'Podcast', icon: 'podcast.png', },
+  { name: 'Movie', icon: 'movie.png', },
+  { name: 'Gaming', icon: 'game.png', },
+  { name: 'Live', icon: 'live.png', },
+  { name: 'Sport', icon: 'sport.png', },
+  { name: 'Fashion', icon: 'fashion.png', },
+  { name: 'Beauty', icon: 'beauty.png', },
+  { name: 'Comedy', icon: 'comedy.png', },
+  { name: 'Gym', icon: 'gym.png', },
+  { name: 'Crypto', icon: 'crypto.png', },
+];
+
+export const demoVideoUrl = '/video/cV2gBU6hKfY';
+export const demoThumbnailUrl = 'https://example.com/default-thumbnail.jpg';
+export const demoVideoTitle = 'Default Video Title';
+export const demoChannelUrl = '/channel/defaultChannelId';
+export const demoChannelTitle = 'Default Channel Title';
+export const demoProfilePicture = 'https://example.com/default-profile-picture.jpg';
